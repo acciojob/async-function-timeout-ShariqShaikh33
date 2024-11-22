576a81984fc37134c657b385ebf03e7d3df9bd69
ShariqShaikh33 <shariqshaikh2705@gmail.com>
2024-11-22 22:16:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your JS code here. If required.
+const button = document.getElementById("btn");
+
+button.addEventListener('click',function (){
+
+	async function display(){
+		
+		let promise = new Promise(function (resolve){
+			setTimeout(function(){resolve(document.getElementById("text").value);},document.getElementById("delay").value);
+		});
+		const outputDiv = document.getElementById("output");
+		outputDiv.innerHTML = await promise;
+	}
+	display();
+	
+	
+
+	
+});
